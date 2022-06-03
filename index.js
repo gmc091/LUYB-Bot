1680e8c5550707e879d6f26ead74dca12211f2ef
gmc091 <giovannicroese1207@gmail.com>
2022-06-03 10:36:12 +0200

    Ep 4

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const DJS = require('discord.js')
+const CH = require('command-handler')
+const path = require('path')
 require('dotenv/config')
 
 const client = new DJS.Client({
-    intents: ['GUILDS']
+  intents: ['GUILDS', 'GUILD_MESSAGES'],
 })
 
 client.on('ready', () => {
-    console.log("The bot is ready!!")
+  console.log('The bot is ready')
+
+  new CH({
+    client,
+    commandsDir: path.join(__dirname, 'commands'),
+  })
 })
 
 client.login(process.env.TOKEN)
