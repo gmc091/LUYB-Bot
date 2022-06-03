e0e5eb4bd658068407c462c6fca1d9e693d0a5b1
gmc091 <giovannicroese1207@gmail.com>
2022-06-03 10:00:22 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const DJS = require('discord.js')
+require('dotenv/config')
+
+const client = new DJS.Client({
+    intents: ['GUILDS']
+})
+
+client.on('ready', () => {
+    console.log("The bot is ready")
+})
+
+client.login(process.env.TOKEN)
